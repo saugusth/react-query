6b5ea14d1044a0a3c979a455f65dbcc4f87b62f4
saugusth <54689875+saugusth@users.noreply.github.com>
2023-03-10 03:14:50 -0600

    query cancelation

--- src/helpers/useLabelsData.js
+++ src/helpers/useLabelsData.js
@@ -1,9 +1,11 @@
 import { useQuery } from "react-query";
 
 export function useLabelsData() {
-  const labelsQuery = useQuery(["labels"], () =>
-    fetch("/api/labels").then((res) => res.json()), {
-      staleTime: 1000 * 60 * 60
+  const labelsQuery = useQuery(
+    ["labels"],
+    ({ signal }) => fetch("/api/labels", { signal }).then((res) => res.json()),
+    {
+      staleTime: 1000 * 60 * 60,
     }
   );
 
--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,9 +1,12 @@
 import { useQuery } from "react-query";
 
 export function useUserData(userId) {
-  const usersData = useQuery(["users", userId], () =>
-    fetch(`/api/users/${userId}`).then((res) => res.json()), {
-      staleTime: 1000 * 60 * 5
+  const usersData = useQuery(
+    ["users", userId],
+    ({ signal }) =>
+      fetch(`/api/users/${userId}`, { signal }).then((res) => res.json()),
+    {
+      staleTime: 1000 * 60 * 5,
     }
   );
 
