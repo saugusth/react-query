c0d75df96437d031185e9e224f2cdb3c18eaeb19
saugusth <54689875+saugusth@users.noreply.github.com>
2023-03-11 13:45:54 -0600

    add issues

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -2,6 +2,7 @@ import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import { useState } from "react";
 import { StatusSelect } from "../components/StatusSelect";
+import { Link } from "react-router-dom";
 
 export default function Issues() {
   const [labels, setLabels] = useState([]);
@@ -10,7 +11,6 @@ export default function Issues() {
     <div>
       <main>
         <section>
-          <h1>Issues</h1>
           <IssuesList labels={labels} status={status} />
         </section>
         <aside>
@@ -31,6 +31,10 @@ export default function Issues() {
             value={status}
             onChange={(event) => setStatus(event.target.value)}
           />
+          <hr />
+          <Link className="button" to="/add">
+            Add Issue
+          </Link>
         </aside>
       </main>
     </div>
